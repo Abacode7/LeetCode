6071b96fc4d2e9c88324d93e4273f48e4c472796
Abraham O <olamipoabraham@gmail.com>
2021-06-25 16:35:51 +0100

    Add Palindrome Improved solution

--- src/EasyLevelQuestions/PalindromeLinkedList.java
+++ src/EasyLevelQuestions/PalindromeLinkedList.java
@@ -9,10 +9,44 @@ public class PalindromeLinkedList {
         System.out.println(isPalindrome(head));
     }
 
+    // Runtime O(n), Space O(1)
+    public static boolean isPalindromeImproved(ListNode head) {
+        ListNode second = head;
+        ListNode first = head;
+
+        while(first != null && first.next != null){
+            second = second.next;
+            first = first.next.next;
+        }
+
+        second = reverseLinkedList(second);
+        first = head;
+
+        while(second != null){
+            if(first.val != second.val) return false;
+            first = first.next;
+            second = second.next;
+        }
+
+        return true;
+    }
+
+    public static ListNode reverseLinkedList(ListNode head){
+        ListNode prev = null;
+        while(head != null){
+            ListNode nextReference = head.next;
+            head.next = prev;
+
+            prev = head;
+            head = nextReference;
+        }
+        return prev;
+    }
 
     // First solution
     // We could also copy List into array and use the
     // two pointer approach
+    // Runtime O(n), Space O(n)
     public static boolean isPalindrome(ListNode head) {
         if(head.next == null) return true;
 
