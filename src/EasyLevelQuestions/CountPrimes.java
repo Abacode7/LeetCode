f89fb81a4faddf031bdbc1e8ba94f3953a4b3017
Abraham O <olamipoabraham@gmail.com>
2021-07-02 00:34:48 +0100

    Add improved CountPrimes solution

--- src/EasyLevelQuestions/CountPrimes.java
+++ src/EasyLevelQuestions/CountPrimes.java
@@ -6,6 +6,36 @@ public class CountPrimes {
         System.out.println(countPrimes(input)); // 2 3 5 7
     }
 
+    // Runtime O(n), Space O(n)
+    public static int countPrimesImproved(int n) {
+        // Sieve of eratosthenes
+        // mark all values from 2 to n as prime.
+        // for all primes from 2 to sqrt(n), mark
+        // their multiples as non-prime.
+        // count all prime values.
+
+        if(n <= 2) return 0;
+
+        boolean[] sieve = new boolean[n];
+        for(int i=2; i<n; i++){
+            sieve[i] = true;
+        }
+
+        for(int i=2; i*i <= n; i++){
+            if(sieve[i]){
+                for(int j = i*i; j < n; j+=i){
+                    sieve[j] = false;
+                }
+            }
+        }
+
+        int count = 0;
+        for(int i=2; i<n; i++){
+            if(sieve[i]) count++;
+        }
+        return count;
+    }
+
     // Time limit exceeded
     public static int countPrimes(int n) {
         int count = 0;
